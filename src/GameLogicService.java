6eaa822809cb551a4e8658097f14a248243f79f4
ymca85 <bhushan.ymca85@gmail.com>
2016-11-28 19:07:42 +0530

    Add files via upload

--- src/GameLogicService.java
+++ src/GameLogicService.java
@@ -9,6 +9,8 @@ public class GameLogicService {
 			message = "rock crushes scissors -> rock wins";
 		} else if(input == "paper" && input2 == "rock") {
 			message = "paper covers rock -> paper wins";
+		} else if (input == "scissors" && input2 == "paper") {
+			message = "scissors cut paper -> scissors wins";
 		}
 		return message;
 	}
