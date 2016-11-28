a27bdd5f6adf44f991c0a752eefbef38a9850055
ymca85 <bhushan.ymca85@gmail.com>
2016-11-28 19:06:30 +0530

    Add files via upload

--- test/GameLogicServicetests.java
+++ test/GameLogicServicetests.java
@@ -14,5 +14,11 @@ public class GameLogicServicetests {
 		GameLogicService gameLogicService = new GameLogicService();
 		assertSame("paper covers rock -> paper wins", gameLogicService.findAndShowWinner("paper", "rock"));
 	}
+	
+	@Test
+	public void testFindAndShowWinnerScissorsCutPaper() {
+		GameLogicService gameLogicService = new GameLogicService();
+		assertSame("scissors cut paper -> scissors wins", gameLogicService.findAndShowWinner("scissors", "paper"));
+	}
 
 }
