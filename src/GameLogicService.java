9a9845f09b13085c41a08f9e002b878330c258ad
ymca85 <bhushan.ymca85@gmail.com>
2016-11-28 19:02:42 +0530

    Add files via upload

--- src/GameLogicService.java
+++ src/GameLogicService.java
@@ -2,6 +2,12 @@
 public class GameLogicService {
 	
 	public String findAndShowWinner(String input, String input2) {
-		return "";
+		
+		String message = "";
+		
+		if(input == "rock" && input2 == "scissors") {
+			message = "rock crushes scissors -> rock wins";
+		}
+		return message;
 	}
 }
