b318a9a020c1cc027009c702dbda9eee8a6a7f69
ymca85 <bhushan.ymca85@gmail.com>
2016-11-29 11:30:28 +0530

    Add files via upload

--- test/GameControllerTests.java
+++ test/GameControllerTests.java
@@ -13,11 +13,19 @@ public class GameControllerTests {
 	}
 	
 	@Test
-	public void testGetFirstInputFromConsole() {
+	public void testGetFirstInputFromConsoleValue1() {
 		GameController gameController = new GameController();
 		Scanner scanner = new Scanner("rock");
 		String inputFromConsole = gameController.getFirstInputFromConsole(scanner);
 		assertSame("rock", inputFromConsole);
+	}
+	
+	@Test
+	public void testGetFirstInputFromConsolevalue2() {
+		GameController gameController = new GameController();
+		Scanner scanner = new Scanner("scissors");
+		String inputFromConsole = gameController.getFirstInputFromConsole(scanner);
+		assertSame("scissors", inputFromConsole);
 	}
 
 }
