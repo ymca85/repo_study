34e177e5f0964cf6c543ddfbc154a7486a8cbfb4
ymca85 <bhushan.ymca85@gmail.com>
2016-11-29 12:07:04 +0530

    Add files via upload

--- src/GameController.java
+++ src/GameController.java
@@ -4,13 +4,29 @@ public class GameController {
 
 	
 	public static void main(String[] args) {
+		
 		GameController gameController = new GameController();
+		InputVaidation inputVaidation = new InputVaidation();
+		GameLogicService gameLogicService = new GameLogicService();
 		
 		gameController.displayGameStartInformation();
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Enter input for Player1: ");
 		String input1 = gameController.getInputFromConsole(scanner);
+		if(!inputVaidation.isInputValid(input1)) {
+			System.out.println(input1
+					+ " is not a valid input, please try again by selecting one of the input as rock/paper/scissors.");
+		}
+		
+		System.out.println("Enter input for Player2: ");
+		String input2 = gameController.getInputFromConsole(scanner);
+		if(!inputVaidation.isInputValid(input2)) {
+			System.out.println(input2
+					+ " is not a valid input, please try again by selecting one of the input as rock/paper/scissors.");
+		}
+		
+		
 	}
 	
 	public void displayGameStartInformation() {
