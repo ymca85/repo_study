3fb013ea270abb3ff295a98d83ec3d3df86524e6
ymca85 <bhushan.ymca85@gmail.com>
2016-11-29 12:52:29 +0530

    Add files via upload

--- test/GameLogicServicetests.java
+++ test/GameLogicServicetests.java
@@ -4,21 +4,21 @@ import org.junit.Test;
 public class GameLogicServicetests {
 
 	@Test
-	public void testFindAndShowWinnerRockVsScissors() {
-		GameLogicService gameLogicService = new GameLogicService();
-		String input1 = "rock";
-		String input2 = "scissors";
-		assertSame(MessageConstants.ROCK_CRUSHES_SCISSORS, gameLogicService.findAndShowWinner(input1, input2));
-	}
-	
-	@Test
 	public void testFindAndShowWinnerRockVsPaper() {
 		GameLogicService gameLogicService = new GameLogicService();
 		String input1 = "rock";
 		String input2 = "paper";
 		assertSame(MessageConstants.PAPER_COVERS_ROCK, gameLogicService.findAndShowWinner(input1, input2));
 	}
-	
+
+	@Test
+	public void testFindAndShowWinnerRockVsScissors() {
+		GameLogicService gameLogicService = new GameLogicService();
+		String input1 = "rock";
+		String input2 = "scissors";
+		assertSame(MessageConstants.ROCK_CRUSHES_SCISSORS, gameLogicService.findAndShowWinner(input1, input2));
+	}
+
 	@Test
 	public void testFindAndShowWinnerPaperVsScissors() {
 		GameLogicService gameLogicService = new GameLogicService();
@@ -26,7 +26,7 @@ public class GameLogicServicetests {
 		String input2 = "scissors";
 		assertSame(MessageConstants.SCISSORS_CUT_PAPER, gameLogicService.findAndShowWinner(input1, input2));
 	}
-	
+
 	@Test
 	public void testFindAndShowWinnerPaperVsRock() {
 		GameLogicService gameLogicService = new GameLogicService();
@@ -34,17 +34,20 @@ public class GameLogicServicetests {
 		String input2 = "rock";
 		assertSame(MessageConstants.PAPER_COVERS_ROCK, gameLogicService.findAndShowWinner(input1, input2));
 	}
-	
+
 	@Test
-	public void testFindAndShowWinnerScissorsCutPaper() {
+	public void testFindAndShowWinnerScissorsVsRock() {
 		GameLogicService gameLogicService = new GameLogicService();
-		assertSame(MessageConstants.SCISSORS_CUT_PAPER, gameLogicService.findAndShowWinner(UserInputs.SCISSORS.toString(), UserInputs.PAPER.toString()));
+		String input1 = "scissors";
+		String input2 = "rock";
+		assertSame(MessageConstants.ROCK_CRUSHES_SCISSORS, gameLogicService.findAndShowWinner(input1, input2));
 	}
-	
+
 	@Test
 	public void testFindAndShowWinnerThisIsTie() {
 		GameLogicService gameLogicService = new GameLogicService();
-		assertSame(MessageConstants.TIE_SAME_OPTIONS_SELECTED, gameLogicService.findAndShowWinner(UserInputs.ROCK.toString(), UserInputs.ROCK.toString()));
+		assertSame(MessageConstants.TIE_SAME_OPTIONS_SELECTED,
+				gameLogicService.findAndShowWinner(UserInputs.ROCK.toString(), UserInputs.ROCK.toString()));
 	}
 
 }
