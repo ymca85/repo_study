75b1aabd846136e9b2088582b184e99cb9b9e67e
ymca85 <bhushan.ymca85@gmail.com>
2016-11-29 11:59:55 +0530

    Add files via upload

--- src/GameController.java
+++ src/GameController.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class GameController {
 
+	
+	public static void main(String[] args) {
+		GameController gameController = new GameController();
+		
+		gameController.displayGameStartInformation();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter input for Player1: ");
+		String input1 = gameController.getInputFromConsole(scanner);
+	}
+	
 	public void displayGameStartInformation() {
 		System.out.println(" Hi .. This is rock-paper-scissor game."
 				+ "\n It will be played by two player. Each player has to enter his choice for rock / paper / scissor."
--- test/GameControllerTests.java
+++ test/GameControllerTests.java
@@ -7,12 +7,6 @@ import org.junit.Test;
 
 public class GameControllerTests {
 
-//	@Test
-//	public void test() {
-//		GameController gameController = new GameController();
-//		gameController.displayGameStartInformation();
-//	}
-	
 	@Test
 	public void testGetInputFromConsoleValue1() {
 		
